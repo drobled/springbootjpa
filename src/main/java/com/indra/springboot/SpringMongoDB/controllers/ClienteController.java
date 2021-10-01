893a49f813c6e2f26097e08dcc81fd562a3e2668
David Robledo <david.robledo@cas-training.com>
2021-10-01 12:02:23 +0200

    Version redis inicial

--- src/main/java/com/indra/springboot/SpringMongoDB/controllers/ClienteController.java
+++ src/main/java/com/indra/springboot/SpringMongoDB/controllers/ClienteController.java
@@ -1,41 +1,19 @@
 package com.indra.springboot.SpringMongoDB.controllers;
 
-import com.indra.springboot.SpringMongoDB.entities.Cliente;
-import com.indra.springboot.SpringMongoDB.entities.Pelicula;
-import com.indra.springboot.SpringMongoDB.repositorios.ClienteRepositorio;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
 
-    @Autowired
-    private ClienteRepositorio clienteRepositorio;
-
     @PostMapping("/alta")
     public String altaCliente(@RequestParam Map<String, String> parametros) {
-        Cliente cliente = new Cliente();
-        cliente.setDireccion(parametros.get("direccion"));
-        cliente.setId(parametros.get("id"));
-        cliente.setNombre(parametros.get("nombre"));
-        clienteRepositorio.save(cliente);
+
         return "Alta cliente correcta";
     }
 
-    @PutMapping("/altaPelicula/{idCliente}")
-    public String altaPelicula(@PathVariable("idCliente") String idCliente, @RequestParam String titulo) {
-        Cliente cliente = clienteRepositorio.findById(idCliente).get();
-        Pelicula pelicula = new Pelicula();
-        pelicula.setTitulo(titulo);
-        if(cliente.getPeliculas() == null)
-            cliente.setPeliculas(new ArrayList<>());
-        cliente.getPeliculas().add(pelicula);
-        clienteRepositorio.save(cliente);
-        return "Alta pelicula correcta";
-    }
+
 
 }
--- src/main/java/com/indra/springboot/SpringMongoDB/repositorios/ClienteRepositorio.java
+++
@@ -1,7 +0,0 @@
-package com.indra.springboot.SpringMongoDB.repositorios;
-
-import com.indra.springboot.SpringMongoDB.entities.Cliente;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ClienteRepositorio extends MongoRepository<Cliente, String> {
-}
