6979bdafa16abc0baed92ad99ed55497525a06e0
David Robledo <david.robledo@cas-training.com>
2021-10-04 09:31:59 +0200

    redis-subida parte de actuator

--- src/main/java/com/indra/springboot/SpringRedis/SpringRedisDbApplication.java
+++ src/main/java/com/indra/springboot/SpringRedis/SpringRedisDbApplication.java
@@ -21,11 +21,5 @@ public class SpringRedisDbApplication {
 		return template;
 	}
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.requestMatcher(EndpointRequest.toAnyEndpoint())
-				.authorizeRequests((requests) -> requests.anyRequest().permitAll());
-		return http.build();
-	}
 
 }
