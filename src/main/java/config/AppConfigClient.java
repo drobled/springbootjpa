1fc7a4eb5c0df173da2a63a24051a5ffe06b20dd
David Robledo <david.robledo@cas-training.com>
2021-10-06 12:55:44 +0200

    cloudeurekaclient-gethost

--- src/main/java/config/AppConfigClient.java
+++ src/main/java/config/AppConfigClient.java
@@ -1,20 +1,29 @@
 package config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class AppConfigClient {
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     public static void main(String[] args) {
         SpringApplication.run(AppConfigClient.class, args);
     }
 
     @RequestMapping("/")
     public String inicio(){
-        return "Hola mundo";
+        List<ServiceInstance> instancias = discoveryClient.getInstances("CLIENTE1");
+        return "Hola mundo " + instancias.get(0).getHost();
     }
 
 }
