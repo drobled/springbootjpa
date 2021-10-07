8ec13efd7173d19131073b82b2fd83dbef408bbd
David Robledo <david.robledo@cas-training.com>
2021-10-07 10:07:35 +0200

    cloudeurekaclientfeign-cliente eureka

--- src/main/java/config/AppConfigClient.java
+++ src/main/java/config/AppConfigClient.java
@@ -5,25 +5,36 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @RestController
+@EnableFeignClients
 public class AppConfigClient {
     @Autowired
-    private DiscoveryClient discoveryClient;
+    SaludoServer saludoServer;
 
     public static void main(String[] args) {
         SpringApplication.run(AppConfigClient.class, args);
     }
 
+    @FeignClient("SaludoServer")
+    interface SaludoServer{
+        @RequestMapping(value = "/",method = RequestMethod.GET)
+        String saludo();
+    }
+
     @RequestMapping("/")
     public String inicio(){
-        List<ServiceInstance> instancias = discoveryClient.getInstances("CLIENTE1");
-        return "Hola mundo " + instancias.get(0).getHost();
+        return "Saludo cliente " + saludoServer.saludo();
     }
 
 }
