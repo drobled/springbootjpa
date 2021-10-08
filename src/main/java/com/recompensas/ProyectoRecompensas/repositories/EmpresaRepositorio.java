04f025b74c3e3d036034e7cc6c6917c59d3b3341
David Robledo <david.robledo@cas-training.com>
2021-10-08 14:15:21 +0200

    recompensas-separacion de objetivos

--- src/main/java/com/recompensas/ProyectoRecompensas/repositories/EmpresaRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.recompensas.ProyectoRecompensas.repositories;
-
-import com.recompensas.ProyectoRecompensas.entities.Empresa;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EmpresaRepositorio extends MongoRepository<Empresa, String> {
-
-}
